770d6149c9d6071cdd44c2e3d8bdd188a41ef98f
sendox2012-debug <sendox2012-debug@users.noreply.github.com>
2026-08-24 04:22:58 +0300

    hi

---
+++ .buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/other_builds/jpeg/arm64-v8a__ndk_target_24/jpeg/CMakeFiles/4.4.2/CMakeSystem.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_HOST_SYSTEM "Linux-7.1.8-zen1-3-zen")
+set(CMAKE_HOST_SYSTEM_NAME "Linux")
+set(CMAKE_HOST_SYSTEM_VERSION "7.1.8-zen1-3-zen")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
+
+include("/home/said/.buildozer/android/platform/android-ndk-r28c/build/cmake/android.toolchain.cmake")
+
+set(CMAKE_SYSTEM "Android-1")
+set(CMAKE_SYSTEM_NAME "Android")
+set(CMAKE_SYSTEM_VERSION "1")
+set(CMAKE_SYSTEM_PROCESSOR "aarch64")
+
+set(CMAKE_CROSSCOMPILING "TRUE")
+
+set(CMAKE_SYSTEM_LOADED 1)
